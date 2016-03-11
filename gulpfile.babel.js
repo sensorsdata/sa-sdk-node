fc65f8ab6afe331b2b942957c88995d1d8f524de
timnew <timnew.wti@gmail.com>
2016-03-11 17:35:23 +0800

    Update build script

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,24 +1,9 @@
 import gulp from 'gulp'
 import bump from 'gulp-bump'
+import babel from 'gulp-babel'
 import start from 'gulp-start-process'
-import loadConfig from 'config-node'
-import createKnex from 'knex'
-import moment from 'moment'
-global.moment = moment
-
-const config = loadConfig({ env: 'common' })()
 
 gulp.task('default', ['spec'])
-
-;['api', 'www', 'server'].forEach((appName) => {
-  gulp.task(appName, [`${appName}:dev`])
-  gulp.task(`${appName}:dev`, (done) => {
-    start(`nodemon bin/${appName} | bunyan`, done)
-  })
-  gulp.task(`${appName}:prod`, (done) => {
-    start(`node bin/${appName}`, done)
-  })
-})
 
 ;['major', 'minor', 'patch'].forEach((type) => {
   gulp.task(`bump:${type}`, ['build'], () =>
@@ -29,50 +14,14 @@ gulp.task('default', ['spec'])
 })
 gulp.task('bump', ['bump:patch'])
 
-function initKnex() {
-  return createKnex({
-    client: 'mysql',
-    connection: {
-      socketPath: '/tmp/mysql.sock',
-      user: 'root',
-    },
-    debug: true,
-  })
-}
-
-gulp.task('db:create', () => {
-  const databaseName = config.database.connection.database
-
-  const knex = initKnex()
-  return knex.raw(`CREATE DATABASE IF NOT EXISTS ${databaseName};`)
-  .then(() => knex.destroy())
-})
-gulp.task('db:drop', () => {
-  const databaseName = config.database.connection.database
-
-  const knex = initKnex()
-  return knex.raw(`DROP DATABASE IF EXISTS ${databaseName};`)
-  .then(() => knex.destroy())
-})
-gulp.task('db:reset', ['db:drop', 'db:create'])
-gulp.task('db:migrate', (done) => {
-  start('knex migrate:latest', done)
-})
-gulp.task('db:rollback', (done) => {
-  start('knex migrate:rollback', done)
-})
-gulp.task('db:rebuild', ['db:reset', 'db:migrate'])
-
-gulp.task('spec', ['spec:local'])
-
-gulp.task('spec:local', (done) => {
-  start('NODE_ENV=test ./node_modules/.bin/mocha --harmony --grep @remote --invert --opts mocha.opts "specs/**/*Spec.js"', done)
+gulp.task('spec', (done) => {
+  start('./node_modules/.bin/mocha --harmony --opts mocha.opts "specs/**/*Spec.js"', done)
 })
 
-gulp.task('spec:remote', (done) => {
-  start('NODE_ENV=test ./node_modules/.bin/mocha --harmony --grep @remote --opts mocha.opts --timeout 5000  "specs/**/*Spec.js"', done)
-})
+gulp.task('build', ['build:babel'])
 
-gulp.task('spec:all', (done) => {
-  start('NODE_ENV=test ./node_modules/.bin/mocha --harmony --opts mocha.opts  --timeout 5000 "specs/**/*Spec.js"', done)
-})
+gulp.task('build:babel', () =>
+  gulp.src('src/*.js')
+		.pipe(babel())
+		.pipe(gulp.dest('lib'))
+)
