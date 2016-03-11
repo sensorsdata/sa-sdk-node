fb621cd1bbdbad239a3c8f6c35ef33c63640022f
timnew <timnew.wti@gmail.com>
2016-03-12 01:17:41 +0800

    Add support to superProperties and simplify the implementation

--- src/SensorsAnalytics.js
+++ src/SensorsAnalytics.js
@@ -1,56 +1,76 @@
 import R from 'ramda'
 import { pascal2Snake, translateKeys, translateTimeStamp } from './translators'
+import { version as PACKAGE_VERSION } from './readPackageVersion'
 
-const convertToEnvelope = translateKeys(pascal2Snake)
+const snakenizeKeys = translateKeys(pascal2Snake)
 
 class SensorsAnalytics {
   constructor(consumer) {
     this.consumer = consumer
+    this.clearSuperProperties()
   }
 
-  track(distinctId, event, properties = {}) {
-    this.internalTrack({ type: 'track', event, distinctId, properties })
+  registerSuperProperties(values = {}) {
+    return Object.assign(this.superProperties, values)
   }
 
-  trackSignup(distinctId, originalId, properties = {}) {
-    this.internalTrack({ type: 'track_signup', event: '$SignUp', distinctId, originalId, properties })
+  clearSuperProperties() {
+    this.superProperties = {
+      $lib: 'Node',
+      $libVersion: PACKAGE_VERSION,
+    }
+
+    return this.superProperties
+  }
+
+  superizeProperties(properties = {}) {
+    return R.merge(this.superProperties, properties)
+  }
+
+  track(distinctId, event, eventProperties) {
+    const properties = this.superizeProperties(eventProperties)
+
+    this.internalTrack('track', { event, distinctId, properties })
+  }
+
+  trackSignup(distinctId, originalId, eventProperties) {
+    const properties = this.superizeProperties(eventProperties)
+
+    this.internalTrack('track_signup', { event: '$SignUp', distinctId, originalId, properties })
   }
 
   profileSet(distinctId, properties) {
-    this.internalTrack({ type: 'profile_set', distinctId, properties })
+    this.internalTrack('profile_set', { distinctId, properties })
   }
 
   profileSetOnce(distinctId, properties) {
-    this.internalTrack({ type: 'profile_set_once', distinctId, properties })
+    this.internalTrack('profile_set_once', { distinctId, properties })
   }
 
   profileIncrement(distinctId, properties) {
-    this.internalTrack({ type: 'profile_increment', distinctId, properties })
+    this.internalTrack('profile_increment', { distinctId, properties })
   }
 
   profileAppend(distinctId, properties) {
-    this.internalTrack({ type: 'profile_append', distinctId, properties })
+    this.internalTrack('profile_append', { distinctId, properties })
   }
 
   profileUnset(distinctId, keys = []) {
-    const properties = R.reduce((result, key) => {
-      result[key] = true
-      return result
-    }, {}, keys)
+    const properties = R.zipObj(keys, R.repeat(true, keys.length))
 
-    this.internalTrack({ type: 'profile_unset', distinctId, originalId: distinctId, properties })
+    this.internalTrack('profile_unset', { distinctId, properties })
   }
 
-  internalTrack({ type, event, distinctId, originalId, properties }) {
+  internalTrack(type, { event, distinctId, originalId, properties }) {
     const time = translateTimeStamp(properties.$time)
 
-    const envelope = convertToEnvelope({
+    const envelope = snakenizeKeys({
       type,
       event,
       time,
       distinctId,
       originalId,
-      properties,
+      properties: snakenizeKeys(properties),
     })
 
     this.consumer.send(envelope)
