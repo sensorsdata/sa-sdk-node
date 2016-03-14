98a161746f3812dfd62e4dd61cd90e6554552e6d
timnew <timnew.wti@gmail.com>
2016-03-15 01:13:02 +0800

    Ignore submit if empty

--- specs/SubmitterSpec.js
+++ specs/SubmitterSpec.js
@@ -1,4 +1,9 @@
+import mockery from 'mockery'
+import fetch from 'node-fetch'
+import fetchMock from 'fetch-mock'
 import Submitter from '../src/Submitter'
+
+fetchMock.useNonGlobalFetch(fetch)
 
 describe('Submitter', () => {
   const url = 'http://sample.cloud.sensorsdata.cn:8006/sa?token=23befde23232e'
@@ -64,6 +69,43 @@ describe('Submitter', () => {
 
     it('should complain if nothing provided', () => {
       expect(() => new Submitter()).to.throw()
+    })
+  })
+
+  xdescribe('onNext', () => {
+    const pattern = '^http://sample.cloud.sensorsdata.cn:8006/'
+    let submitter = null
+
+    beforeEach(() => {
+      mockery.enable({ useCleanCache: true })
+      mockery.registerMock('node-fetch', fetchMock.mock(pattern, 'POST', 200).getMock())
+
+      submitter = new Submitter({ url, gzip: false })
+    })
+
+    afterEach(() => {
+      mockery.deregisterMock('node-fetch')
+      mockery.disable()
+    })
+
+    it('should submit single data', async () => {
+      const value = await submitter.onNext({ event: 'event' })
+      expect(fetchMock.called(pattern)).to.be.true
+    })
+
+    it('should submit array data', async () => {
+      await submitter.onNext([{ event: 'event1' }, { event: 'event2' }])
+      expect(fetchMock.called(pattern)).to.be.true
+    })
+
+    it('should ignore if no data provided', async () => {
+      await submitter.onNext(null)
+      expect(fetchMock.called(pattern)).to.be.false
+    })
+
+    it('should ignore if empty array provided', async () => {
+      await submitter.onNext([])
+      expect(fetchMock.called(pattern)).to.be.false
     })
   })
 })
