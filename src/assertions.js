dbad6d09a52f50e9f21fe3cee02be98a0b3d614d
timnew <timnewcnyn@gmail.com>
2016-07-21 16:31:15 +0800

    Better debug message

--- src/assertions.js
+++ src/assertions.js
@@ -1,8 +1,13 @@
 import R from 'ramda'
+import createDebug from 'debug'
+
+const debug = createDebug('sa:assertions')
 
 const KEY_PATTERN = /^((?!^distinct_id$|^original_id$|^time$|^properties$|^id$|^first_id$|^second_id$|^users$|^events$|^event$|^user_id$|^date$|^datetime$)[a-zA-Z_$][a-zA-Z\d_$]{0,99})$/
 
 export function checkExists(value, name = 'value') {
+  debug('checkExists: %s => %j', name, value)
+
   if (typeof(value) !== 'string') {
     throw new Error(`${name} must be a string`)
   }
@@ -19,6 +24,8 @@ export function checkExists(value, name = 'value') {
 }
 
 export function checkPattern(value, name = 'value') {
+  debug('checkPattern: %s', value)
+
   checkExists(value, name)
 
   if (!KEY_PATTERN.exec(value)) {
@@ -29,6 +36,7 @@ export function checkPattern(value, name = 'value') {
 }
 
 export function checkValueType(key) {
+  debug('checkValyeType: this[%s]', key)
   const value = this[key]
   switch (R.type(value)) {
     case 'Number':
@@ -43,6 +51,7 @@ export function checkValueType(key) {
 }
 
 export function checkValueIsNumber(key) {
+  debug('checkValueIsNumber: this[%s]', key)
   const value = this[key]
   if (typeof value !== 'number') {
     throw new Error(`Property ${key} must be number: ${value}`)
@@ -50,6 +59,8 @@ export function checkValueIsNumber(key) {
 }
 
 export function checkIsStringArray(value, name = 'Value') {
+  debug('checkIsStringArray: %s => %j', name, value)
+
   if (!Array.isArray(value) || !R.all(R.is(String), value)) {
     throw new Error(`${name} must be a array of string: ${value}`)
   }
@@ -57,11 +68,14 @@ export function checkIsStringArray(value, name = 'Value') {
 }
 
 export function checkValueIsStringArray(key) {
+  debug('checkValueIsStringArray: this[%s]', key)
+
   const value = this[key]
-  checkIsStringArray(value)
+  checkIsStringArray(value, key)
 }
 
 export function checkProperties(obj, checker) {
+  debug('checkProperties: %j', obj)
   R.forEach(R.bind(checker, obj), R.keys(obj))
 
   return obj
