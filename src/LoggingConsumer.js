4ccdfbeaa6c0efbd12551fb381af12baa079b3aa
weiyi <weiyi@sensorsdata.cn>
2019-12-04 10:58:37 +0800

    1.19

--- src/LoggingConsumer.js
+++ src/LoggingConsumer.js
@@ -1,55 +1,53 @@
 /**
  * Created by m1911 on 17/1/12.
  */
-import { createLogger, format, transports } from "winston";
-import "winston-daily-rotate-file";
+import { createLogger, format, transports } from 'winston'
+import 'winston-daily-rotate-file'
 
 export default class LoggingConsumer {
   constructor(filePath, pm2Mode) {
-    this.currentFileName = null;
-    this.logger = null;
-    this.logName = "SensorsData.Analytics.LoggingConsumer";
-    this.filePrefix = "/service.log.";
-    this.initLoggingConsumer(filePath, pm2Mode);
+    this.currentFileName = null
+    this.logger = null
+    this.logName = 'SensorsData.Analytics.LoggingConsumer'
+    this.filePrefix = '/service.log.'
+    this.initLoggingConsumer(filePath, pm2Mode)
   }
 
   initLoggingConsumer(filePath, pm2Mode) {
-    this.initLoggingConsumer(filePath);
+    this.initLoggingConsumer(filePath)
   }
 
   initLoggingConsumer(filePath) {
-    const myFormat = format.printf(({ message }) => {
-      return message;
-    });
+    const myFormat = format.printf(({ message }) => message)
     const saLogConfiguration = {
       transports: [
         new transports.DailyRotateFile({
-          filename: filePath + this.filePrefix + "%DATE%",
-          datePattern: "YYYYMMDD",
+          filename: `${filePath + this.filePrefix}%DATE%`,
+          datePattern: 'YYYYMMDD',
           format: myFormat,
-          level: "info",
-          zippedArchive: true
-        })
-      ]
-    };
-    this.logger = createLogger(saLogConfiguration);
+          level: 'info',
+          zippedArchive: false,
+        }),
+      ],
+    }
+    this.logger = createLogger(saLogConfiguration)
   }
 
   doFlush() {}
 
   send(msg) {
     try {
-      this.logger.info(JSON.stringify(msg));
+      this.logger.info(JSON.stringify(msg))
     } catch (e) {
-      console.error(e);
+      console.error(e)
     }
   }
 
   close() {
-    log4js.shutdown(() => {});
+    this.logger.close()
   }
 
   constructFilePath() {
-    return this.filePrefix;
+    return this.filePrefix
   }
 }
