78947adda133e70d325c8334d1ef9f8527346ee6
SensorsDataHF <hf@SensorsDataHFdeMacBook-Pro.local>
2020-08-11 11:47:55 +0800

    update version

--- specs/nw.test.js
+++ specs/nw.test.js
@@ -1,49 +1,47 @@
 /**
  * Created by m1911 on 16/12/14.
  */
-const SensorsdataAnanlytics = require('../lib/SensorsAnalytics').default
+const SensorsdataAnanlytics = require("../lib/SensorsAnalytics").default;
 
-const sa = new SensorsdataAnanlytics()
+const sa = new SensorsdataAnanlytics();
 
-const URL = 'https://newsdktest.datasink.sensorsdata.cn/sa?project=weiyi&token=5a394d2405c147ca'
+const URL = "http://127.0.0.1/sa";
 
 // sa.initLoggingConsumer(__dirname)
 sa.initNWConsumer({
-    url: URL, // 数据接收地址
-    cachePath: __dirname, // 缓存路径
-    timeout: 1 * 1000 // 发送数据超时时间
-})
-sa.disableReNameOption()
-    // sa.submitTo(URL, {
-    //   gzip: true,
-    //   mode: 'debug',
-    //   timeout: 10 * 1000,
-    // })
+  url: URL, // 数据接收地址
+  cachePath: __dirname, // 缓存路径
+  timeout: 1 * 1000, // 发送数据超时时间
+});
+sa.disableReNameOption();
+// sa.submitTo(URL, {
+//   gzip: true,
+//   mode: 'debug',
+//   timeout: 10 * 1000,
+// })
 sa.registerSuperProperties({
-    // $app_version: '0.3.0',
-    env: process.env.NODE_ENV || 'default',
-})
-sa.profileSet('111', {
-        aa: 'bb',
-    })
-    // sa.trackSignup('111', '222')
-    // sa.track('111', 'aaa', {})
-    // 商品 Type
-const itemType = 'book'
-    // 商品 ID
-const itemId = '0321714113'
-    // 商品信息
+  // $app_version: '0.3.0',
+  env: process.env.NODE_ENV || "default",
+});
+sa.profileSet("111", {
+  aa: "bb",
+});
+// sa.trackSignup('111', '222')
+// sa.track('111', 'aaa', {})
+// 商品 Type
+const itemType = "book";
+// 商品 ID
+const itemId = "0321714113";
+// 商品信息
 const properties = {
-        name: 'C++ Primer',
-        c_name: 'C++ Primer',
-        price: 31.54,
-    }
-    // 添加商品
-sa.itemSet(itemType, itemId, properties)
-    // 删除商品
-sa.itemDelete(itemType, itemId)
-
-
+  name: "C++ Primer",
+  c_name: "C++ Primer",
+  price: 31.54,
+};
+// 添加商品
+sa.itemSet(itemType, itemId, properties);
+// 删除商品
+sa.itemDelete(itemType, itemId);
 
 // // module.exports = sa
 // let n = 10
